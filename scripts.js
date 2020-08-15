e407ae0a9d1329cdac6beb08cd5386a7a8415061
Garrett Miller <garrettralphmiller@gmail.com>
2020-08-15 18:03:08 -0400

    Display current temperature

--- scripts.js
+++ scripts.js
@@ -2,13 +2,22 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=ec05db5f314ba69e685a65600301fbc2', true)
+request.open('GET', 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&units=imperial&exclude=hourly&appid=ec05db5f314ba69e685a65600301fbc2', true)
 
 request.onload = function () {
   var data = JSON.parse(this.response);
+  console.log(data)
+
+  // Create and append data for current temperature
+  var temperature = data.current.temp;
+  const container = document.getElementById('current-temp');
+  container.textContent = temperature;
   
-  console.log (data)
+  
 }
 
 // Send request
 request.send()
+
+
+
